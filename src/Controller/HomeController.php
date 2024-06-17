a6ebf7454febd4a196791695945f8e759db40532
Hara Ki ri <115027871+ClaudioYammi@users.noreply.github.com>
2024-06-17 18:23:21 +0300

    new

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VoitureRepository;
 
 class HomeController extends AbstractController
 {
@@ -14,5 +15,30 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/voitures/statistics", name="voitures_statistics")
+     */
+    public function getVoitureStatistics(VoitureRepository $voitureRepository): Response
+    {
+        $voitures = $voitureRepository->findAll();
+        $enPanne = 0;
+        $enBonEtat = 0;
+
+        foreach ($voitures as $voiture) {
+            foreach ($voiture->getEtatvoitures() as $etat) {
+                if ($etat->getEtat() === 'Enpanne') {
+                    $enPanne++;
+                } elseif ($etat->getEtat() === 'Enbonétat') {
+                    $enBonEtat++;
+                }
+            }
+        }
+
+        return $this->json([
+            'en_panne' => $enPanne,
+            'en_bon_etat' => $enBonEtat,
+        ]);
     }
 }
